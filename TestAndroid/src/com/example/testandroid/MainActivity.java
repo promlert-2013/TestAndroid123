ed8f2d5c99ac1859282d729f10c5dea2df5679bf
promlert <promlert@gmail.com>
2014-07-18 12:47:01 +0700

    edit MainActivity set text to TextView

--- TestAndroid/src/com/example/testandroid/MainActivity.java
+++ TestAndroid/src/com/example/testandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView tv = (TextView) this.findViewById(R.id.message);
+        tv.setText("Hello");
     }
 }
